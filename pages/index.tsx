3fd3524cd433e374d176ccadd019aa3e2deb277c
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-09-27 03:24:25 +0500

    error remove

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Layout from "./Components/L/Layout";
 import Herobanner from "./Components/H/Herobanner";
 import ChannelList from "./Components/C/ChannelList";
-import BreadCrumbs from "./Components/B/BreadCrumbs";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,7 +14,10 @@ export default function Home() {
       <Layout>
         <Herobanner />
         <div className='px-12 py-12'>
-          <BreadCrumbs bredName="Live Channel" LinkTxt="See All" URL="/SubPages/live" />
+          <div className='flex flex-wrap items-center justify-between px-2 py-4 border-l-[8px] border-[#ff59bf]'>
+            <h2 className='text-2xl font-bold'>Live Channel</h2>
+            <Link href="/SubPages/live" className='text-[#ff0000] font-bold hover:text-[#151244ce]'>See All &rarr;</Link>
+          </div>
           <ChannelList />
         </div>
       </Layout>
