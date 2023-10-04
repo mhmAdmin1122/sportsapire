f7526337a7e8d09e3e05e6120267bd7f2023efe0
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-10-05 01:55:51 +0500

    mobile menue added

--- pages/Components/N/NavBar.tsx
+++ pages/Components/N/NavBar.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '@/public/img/LOGO.png'
 import Link from 'next/link'
 import ImageBox from '../I/ImageBox'
 import { MdOutlineMenuBook } from 'react-icons/md'
 
 const NavBar = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = (event: any) => {
+        setIsActive(current => !current);
+    };
+
     return (
-        <nav className='navbar_site_box flex flex-wrap items-center justify-between px-10 bg-[#0f0d31] h-[80px] text-[#fff] w-full'>
+        <nav className='navbar_site_box relative flex flex-wrap items-center justify-between px-10 bg-[#0f0d31] h-[80px] text-[#fff] w-full'>
 
             {/* Logo */}
             <Link href={"/"} className="w-[120px] h-[80px] logo_pic">
@@ -21,8 +27,13 @@ const NavBar = () => {
                 <Link href={"/SubPages/help"}><li className=''>Help</li></Link>
             </ul>
 
-            <button className='menuIcon_MobileView_Tabs text-2xl font-bold'><MdOutlineMenuBook /></button>
-
+            <button className='menuIcon_MobileView_Tabs text-2xl font-bold' onClick={handleClick}><MdOutlineMenuBook /></button>
+            {isActive && <div className="mobileMenuList">
+                <Link href={"/SubPages/live"}><li className=''>Live</li></Link>
+                <Link href={"/SubPages/shop"}><li className=''>Shop</li></Link>
+                <Link href={"/SubPages/schedule"}><li className=''>Schedule</li></Link>
+                <Link href={"/SubPages/help"}><li className=''>Help</li></Link>
+            </div>}
         </nav>
     )
 }
